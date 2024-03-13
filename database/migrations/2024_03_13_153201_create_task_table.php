47ee21507dacaedefa8085a651af48818f81e609
Imanuel Saragih <imanuelsaragih07@gmail.com>
2024-03-13 22:39:19 +0700

    Update commit

---
+++ database/migrations/2024_03_13_153201_create_task_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('due_at')->nullable();
+            $table->boolean('status')->nullable()->comment('NULL: Not Started, FALSE: In Progress, TRUE: completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
